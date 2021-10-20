ac38c8dafc728abc4050d2275f3a5fccf1e6df19
Fernando <fernando.gomes.siqueira@gmail.com>
2021-10-20 12:47:34 -0300

    Finalizando parte lógica

--- app/src/main/java/com/example/autenticadorqrcode/MainActivity.java
+++ app/src/main/java/com/example/autenticadorqrcode/MainActivity.java
@@ -1,13 +1,52 @@
 package com.example.autenticadorqrcode;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnScan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnScan = (Button) findViewById(R.id.btnscan);
+        final Activity activity = this;
+
+        btnScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                IntentIntegrator integrator = new IntentIntegrator(activity);
+                integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
+                integrator.setPrompt("Camera Scanner");
+                integrator.initiateScan();
+
+            }
+        });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode,resultCode,data);
+        if (result != null) {
+            if (result.getContents() != null) {
+                alert(result.getContents());}
+            else {alert("Codigo Autenticador está com Inváilo, contate o Suporte");}
+
+
+        }else {super.onActivityResult(requestCode, resultCode, data);} }
+
+    private void alert(String msg){
+        Toast.makeText(getApplicationContext(),msg,Toast.LENGTH_LONG).show();
+    }
+
 }
